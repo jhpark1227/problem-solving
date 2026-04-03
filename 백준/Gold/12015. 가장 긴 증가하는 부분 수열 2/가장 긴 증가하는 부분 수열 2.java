f69db49ce9d57bb10f4b85bb2b6991d8a891dc1e
Junhyeok Park <111430281+jhpark1227@users.noreply.github.com>
2026-04-03 18:48:08 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 632 ms, Memory: 147096 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
@@ -1,37 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
-public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] input = new int[n];
-        for(int i=0;i<n;i++){
-            input[i] = sc.nextInt();
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bufferedReader.readLine());
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            list.add(Integer.parseInt(stringTokenizer.nextToken()));
         }
-        List<Integer> lis = new ArrayList<>();
-        lis.add(input[0]);
-        for(int i=1;i<input.length;i++){
-            if(input[i] > lis.get(lis.size()-1)){
-                lis.add(input[i]);
-            } else if(input[i] < lis.get(lis.size()-1)){
-                int index = binarySearch(lis, input[i]);
-                lis.set(index, input[i]);
+        List<Integer> sequence = new ArrayList<>();
+        sequence.add(list.get(0));
+        for (int i = 1; i < list.size(); i++) {
+            int now = list.get(i);
+            if (now > sequence.get(sequence.size() - 1)) {
+                sequence.add(now);
+            } else {
+                sequence.set(binarySearch(sequence, now), now);
             }
         }
-        System.out.println(lis.size());
+        System.out.print(sequence.size());
     }
 
-    static int binarySearch(List<Integer> lis, int value){
-        int left = 0, right = lis.size()-1;
-
-        while(left < right){
+    private static int binarySearch(List<Integer> sequence, int value) {
+        int left = 0;
+        int right = sequence.size() - 1;
+        while (left < right) {
             int mid = (left + right) / 2;
-            if(lis.get(mid) < value){
-                left = mid+1;
-            } else if(lis.get(mid) > value){
+            if (sequence.get(mid) > value) {
                 right = mid;
+            } else if (sequence.get(mid) < value) {
+                left = mid + 1;
             } else {
                 return mid;
             }
