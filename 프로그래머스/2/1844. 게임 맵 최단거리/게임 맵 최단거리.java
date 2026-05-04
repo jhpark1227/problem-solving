aaecb23a11d13426e850d41f60e2d31622141d34
Junhyeok Park <111430281+jhpark1227@users.noreply.github.com>
2026-05-04 17:43:18 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 9.48 ms, Memory: 55.6 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,42 +1,40 @@
 import java.util.*;
 
 class Solution {
+    private int[] dy = {1, -1, 0, 0};
+    private int[] dx = {0, 0, 1, -1};
+    
     public int solution(int[][] maps) {
-        return dfs(0, 0, maps);
+        return bfs(maps);
     }
     
-    private int dfs(int y, int x, int[][] maps){
-        boolean[][] visited = new boolean[maps.length][maps[0].length];
+    private int bfs(int[][] maps) {
         Queue<Node> q = new LinkedList<>();
-        q.add(new Node(y, x, 1));
-        visited[y][x] = true;
-        int[] dx = {1, -1, 0, 0};
-        int[] dy = {0, 0, 1, -1};
-        while(!q.isEmpty()){
+        q.add(new Node(0, 0, 1));
+        boolean[][] isVisited = new boolean[maps.length][maps[0].length];
+        isVisited[0][0] = true;
+        while(!q.isEmpty()) {
             Node now = q.poll();
-            for(int i=0;i<4;i++){
+            if (now.y == maps.length - 1 && now.x == maps[0].length - 1) {
+                return now.depth;
+            }
+            for(int i=0;i<dy.length;i++) {
                 int newY = now.y + dy[i];
                 int newX = now.x + dx[i];
-                if(newY >= 0 && newY < maps.length &&
-                   newX >= 0 && newX < maps[0].length){
-                    if(!visited[newY][newX] && maps[newY][newX] == 1){
-                        if(newY == maps.length-1 && newX == maps[0].length-1){
-                            return now.depth + 1;
-                        }
-                        q.add(new Node(newY, newX, now.depth+1));
-                        visited[newY][newX] = true;
-                    }
-                }
+                if (newY < 0 || newX < 0 || newY >= maps.length || newX >= maps[0].length || isVisited[newY][newX] || maps[newY][newX] == 0) continue;
+                q.add(new Node(newY, newX, now.depth+1));
+                isVisited[newY][newX] = true;
             }
         }
         return -1;
     }
     
-    private static class Node{
+    private class Node {
         int y;
         int x;
         int depth;
-        Node(int y, int x, int depth){
+        
+        Node(int y, int x, int depth) {
             this.y = y;
             this.x = x;
             this.depth = depth;
