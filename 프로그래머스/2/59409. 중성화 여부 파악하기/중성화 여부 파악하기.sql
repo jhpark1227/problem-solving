a6c77bbc73261ac6fc11c914b2cf308de418e1b6
Junhyeok Park <111430281+jhpark1227@users.noreply.github.com>
2026-05-04 19:12:20 +0900

    [level 2] Title: 중성화 여부 파악하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
+++ 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
@@ -1,7 +1,11 @@
-SELECT 
+SELECT
     ANIMAL_ID,
-    NAME, 
-    IF(SEX_UPON_INTAKE LIKE 'Neutered%'
-        OR SEX_UPON_INTAKE LIKE 'Spayed%', 'O', 'X') AS 중성화
+    NAME,
+    CASE 
+        WHEN SEX_UPON_INTAKE LIKE '%Neutered%'
+            OR SEX_UPON_INTAKE LIKE '%Spayed%'
+        THEN 'O'
+        ELSE 'X'
+    END AS 중성화
 FROM ANIMAL_INS
 ORDER BY ANIMAL_ID
