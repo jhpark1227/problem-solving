5db10d9da9677619273f50624d76184027d32925
Junhyeok Park <111430281+jhpark1227@users.noreply.github.com>
2026-05-05 16:01:00 +0900

    [level 2] Title: 땅따먹기, Time: 23.52 ms, Memory: 88.1 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
@@ -1,22 +1,19 @@
 class Solution {
     int solution(int[][] land) {
-        for(int i=1;i<land.length;i++){
-            for(int j=0;j<4;j++){
-                int max = 0;
-                for(int k=0;k<4;k++){
-                    if(j==k) continue;
-                    if(land[i-1][k] > max) max = land[i-1][k]; 
+        int[][] dp = new int[land.length][land[0].length];
+        dp[0] = land[0];
+        for(int i=1;i<land.length;i++) {
+            for(int j=0;j<4;j++) {
+                for(int k=0;k<4;k++) {
+                    if (k == j) continue;
+                    dp[i][j] = Math.max(dp[i][j], dp[i-1][k] + land[i][j]);
                 }
-                land[i][j] += max;
             }
         }
-        
         int answer = 0;
-        for(int i=0;i<4;i++){
-            int x = land[land.length-1][i];
-            if(x > answer) answer = x;
+        for(int i=0;i<4;i++) {
+            answer = Math.max(answer, dp[dp.length - 1][i]);
         }
-        
         return answer;
     }
 }
