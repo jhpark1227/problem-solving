9fd01ab08c1cae346a19a2d18776b5e506bb4ee1
Junhyeok Park <111430281+jhpark1227@users.noreply.github.com>
2026-05-05 18:15:12 +0900

    [level 2] Title: 숫자 블록, Time: 1046.30 ms, Memory: 54.8 MB -BaekjoonHub

--- 프로그래머스/2/12923. 숫자 블록/숫자 블록.java
+++ 프로그래머스/2/12923. 숫자 블록/숫자 블록.java
@@ -1,21 +1,19 @@
 class Solution {
     public int[] solution(long begin, long end) {
-        int[] dp = new int[(int)(end-begin+1)];
-        for(long i=begin;i<=end;i++){
+        int[] blocks = new int[(int)(end - begin + 1)];
+        for(long i=begin;i<=end;i++) {
             int max = 0;
-            for(long j=1;j<=Math.sqrt(i);j++){
-                if(i % j == 0 && i / j != 1){
-                    if(j == 1) max = 1;
-                    else{
-                        if(j > max && j <= 10000000) max = (int)j;
-                        if(i/j > max && i/j <= 10000000) max = (int)(i/j);
-                    } 
+            for(int j=1;j<=Math.sqrt(i);j++) {
+                if (i % j != 0) continue;
+                if(j != i && j <= 10000000) {
+                    max = Math.max(max, (int)j);      
                 }
-                dp[(int)(i-begin)] = max;
+                if (i / j != i && i / j <= 10000000) {
+                    max = Math.max(max, (int)(i / j));      
+                }
             }
+            blocks[(int)(i - begin)] = max;
         }
-            
-        return dp;
+        return blocks;
     }
 }
-
