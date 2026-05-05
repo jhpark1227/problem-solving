6b047ff8ee7814fa035aeb4b6fad17b0e48779c3
Junhyeok Park <111430281+jhpark1227@users.noreply.github.com>
2026-05-05 19:10:22 +0900

    [level 2] Title: 숫자의 표현, Time: 0.89 ms, Memory: 54.7 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -1,14 +1,21 @@
 class Solution {
     public int solution(int n) {
+        int[] sum = new int[10001];
+        for(int i=1;i<sum.length;i++) {
+            sum[i] = sum[i - 1] + i;
+        }
         int count = 0;
-        for(int i=1;i<=n;i++){
-            int sum = 0;
-            for(int j=i;j<=n;j++){
-                sum += j;
-                if(sum==n){
-                    count++;
-                    break;
-                }else if(sum > n)break;
+        int left = 0;
+        int right = 1;
+        while(left < right) {
+            if (sum[right] - sum[left] == n) {
+                count++;
+            }
+            if (sum[right] - sum[left] <= n) {
+                right++;
+            }
+            if (sum[right] - sum[left] > n) {
+                left++;
             }
         }
         return count;
