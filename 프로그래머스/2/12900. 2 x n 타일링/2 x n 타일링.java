11b99b88bb32fe219b0c73bb74dcc1a6ada2fa8d
Junhyeok Park <111430281+jhpark1227@users.noreply.github.com>
2026-05-05 14:03:27 +0900

    [level 2] Title: 2 x n 타일링, Time: 2.19 ms, Memory: 54.5 MB -BaekjoonHub

--- 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.java
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.java
@@ -1,10 +1,10 @@
 class Solution {
     public int solution(int n) {
-        int[] dp = new int[n+1];
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
         dp[1] = 1;
-        dp[2] = 2;
-        for(int i=3;i<=n;i++){
-            dp[i] = (dp[i-2] + dp[i-1]) % 1000000007;
+        for(int i=2;i<=n;i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 1_000_000_007;
         }
         return dp[n];
     }
