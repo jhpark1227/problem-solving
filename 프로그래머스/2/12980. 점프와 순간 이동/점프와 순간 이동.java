3f5a72344fe3448207097ffbce9bb4d3d919c97a
Junhyeok Park <111430281+jhpark1227@users.noreply.github.com>
2026-05-06 16:51:38 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.03 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
@@ -2,15 +2,15 @@ import java.util.*;
 
 public class Solution {
     public int solution(int n) {
-        int count = 0;
-        while(n > 0){
-            if(n % 2 ==0) n /= 2;
-            else {
-                n--;
-                count++;
+        int cost = 0;
+        while(n != 0) {
+            if (n % 2 == 0) {
+                n /= 2;
+                continue;
             }
+            n--;
+            cost++;
         }
-        
-        return count;
+        return cost;
     }
 }
