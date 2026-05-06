394e23a317164f0c5c30a1dafeddf4c167e5b958
Junhyeok Park <111430281+jhpark1227@users.noreply.github.com>
2026-05-06 15:23:00 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 15.51 ms, Memory: 87.1 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -1,20 +1,22 @@
 class Solution {
     public String solution(String s) {
-        String result = "";
-        if(Character.isLowerCase(s.charAt(0))) result += (char)(s.charAt(0)+('A'-'a'));
-        else result += s.charAt(0);
-        
-        for(int i=1;i<s.length();i++){
+        String answer = "";
+        for(int i=0;i<s.length();i++) {
             char c = s.charAt(i);
-        
-            if(s.charAt(i-1)==' ' && c!=' '){
-                 if(Character.isLowerCase(c)) result += (char)(c+'A'-'a');    
-                 else result += c;
-            } else{
-                if(Character.isUpperCase(c)) result += (char)(c-('A'-'a'));
-                else result += c;
+            if (i==0 || s.charAt(i - 1) == ' ') {
+                if (c >= 'a' && c <= 'z')  {
+                    answer += (char)(c + 'A' - 'a');
+                    continue;
+                }
+                answer += c;
+                continue;
+            }    
+            if (c >= 'A' && c <= 'Z') {
+                answer += (char)(c - ('A' - 'a'));
+                continue;
             }
+            answer += c;
         }
-        return result;
+        return answer;
     }
 }
