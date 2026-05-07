01ceeb9eb6f04a97c19a4b9b3bc4e9d362fa64d8
Junhyeok Park <111430281+jhpark1227@users.noreply.github.com>
2026-05-07 16:51:09 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 8.47 ms, Memory: 85 MB -BaekjoonHub

--- 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.java
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.java
@@ -1,47 +1,48 @@
 class Solution {
     public int solution(int m, int n, String[] board) {
         char[][] blocks = new char[m][n];
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
+        for(int i=0;i<m;i++) {
+            for(int j=0;j<n;j++) {
                 blocks[i][j] = board[i].charAt(j);
             }
         }
-        int count = 0;
-        while(true){
-            boolean stop = true;
-            boolean deleteBlocks[][] = new boolean[m][n];
-            for(int i=0;i<m-1;i++){
-                for(int j=0;j<n-1;j++){
-                    if(blocks[i][j] != '-' &&
-                            blocks[i][j] == blocks[i+1][j] &&
-                            blocks[i][j] == blocks[i][j+1] &&
-                            blocks[i][j] == blocks[i+1][j+1]){
-                        deleteBlocks[i][j] = true;
-                        deleteBlocks[i][j+1] = true;
-                        deleteBlocks[i+1][j] = true;
-                        deleteBlocks[i+1][j+1] = true;
-                        stop = false;
+        while(true) {
+            boolean[][] deleted = new boolean[m][n];
+            int count = 0;
+            for(int i=1;i<m;i++) {
+                for(int j=1;j<n;j++) {
+                    if (blocks[i][j] != 'x' && blocks[i - 1][j - 1] == blocks[i][j] && blocks[i - 1][j] == blocks[i][j] && blocks[i][j - 1] == blocks[i][j]) {
+                        deleted[i-1][j-1] = true;
+                        deleted[i-1][j] = true;
+                        deleted[i][j-1] = true;
+                        deleted[i][j] = true;
+                        count++;
                     }
                 }
             }
-            if(stop) break;
-
-            for(int i=0;i<n;i++){
-                int index = m-1;
-                for(int j=m-1;j>=0;j--){
-                    if(deleteBlocks[j][i]){
+            for(int i=0;i<n;i++) {
+                int index = m - 1;
+                for(int j=m-1;j>=0;j--) {
+                    if (deleted[j][i]) {
                         continue;
                     }
                     blocks[index][i] = blocks[j][i];
                     index--;
                 }
-                for(int j=index;j>=0;j--){
-                    blocks[j][i] = '-';
-                    count++;
+                for(int j=index;j>=0;j--) {
+                    blocks[j][i] = 'x';
+                }
+            }
+            if (count ==0) break;
+        }
+        int answer = 0;
+        for(int i=0;i<m;i++) {
+            for(int j=0;j<n;j++) {
+                if (blocks[i][j] == 'x') {
+                    answer++;
                 }
             }
         }
-
-        return count;
+        return answer;
     }
 }
