558a3f6a9be213f90d41580186028451c2456558
Junhyeok Park <111430281+jhpark1227@users.noreply.github.com>
2026-05-07 14:24:18 +0900

    [level 2] Title: 짝지어 제거하기, Time: 32.20 ms, Memory: 68.3 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -5,14 +5,14 @@ class Solution
     public int solution(String s)
     {
         Stack<Character> stack = new Stack<>();
-        for(char c : s.toCharArray()){
-            if(stack.isEmpty()) stack.add(c);
-            else{
-                if(stack.peek() == c) stack.pop();
-                else stack.add(c);
+        char[] arr = s.toCharArray();
+        for(int i=0;i<arr.length;i++) {
+            if (!stack.isEmpty() && stack.peek() == arr[i]) {
+                stack.pop();
+                continue;
             }
+            stack.add(arr[i]);
         }
-        if(stack.isEmpty()) return 1;
-        return 0;
+        return stack.isEmpty() ? 1 : 0;
     }
 }
