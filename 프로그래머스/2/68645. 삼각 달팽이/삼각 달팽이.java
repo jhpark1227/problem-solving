73fbf36ba20fe77d5414a3dba5120c21e1e5faea
Junhyeok Park <111430281+jhpark1227@users.noreply.github.com>
2026-09-22 17:06:19 +0900

    [level 2] Title: 삼각 달팽이, Time: 10.07 ms, Memory: 136 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
@@ -1,36 +1,38 @@
-public class Solution {
-    private static final int[] dx = {0,1,-1};
-    private static final int[] dy = {1,0,-1};
-
-    public static int[] solution(int n){
+class Solution {
+    
+    private int[] dy = {1, 0, -1};
+    private int[] dx = {0, 1, -1};
+    
+    public int[] solution(int n) {
         int[][] triangle = new int[n][n];
-        int v = 1;
-        int x = 0;
         int y = 0;
-        int d = 0;
-
+        int x = 0;
+        int value = 1;
+        int direction = 0;
         while (true) {
-            triangle[y][x] = v++;
-            int nx = x+dx[d];
-            int ny = y+dy[d];
-            if(nx == n || ny == n ||nx == -1 ||ny == -1 || triangle[ny][nx] != 0) {
-                d = (d + 1) % 3;
-                nx = x + dx[d];
-                ny = y + dy[d];
-                if (nx == n || ny == n || nx == -1 || ny == -1 || triangle[ny][nx] != 0) break;
+            triangle[y][x] = value++;
+            int nextY = y + dy[direction];
+            int nextX = x + dx[direction];
+            if (nextY == n || nextY == -1 || nextX == n || nextX == -1 || triangle[nextY][nextX] != 0) {
+                direction = (direction + 1) % 3;
+                nextY = y + dy[direction];
+                nextX = x + dx[direction];
+                if (nextY == n || nextY == -1 || nextX == n || nextX == -1 || triangle[nextY][nextX] != 0) {
+                    break;
+                }
             }
-            x = nx;
-            y = ny;
+            y = nextY;
+            x = nextX;
         }
-
-        int[] result = new int[v - 1];
+        
+        int[] answer = new int[value-1];
         int index = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j <= i; j++) {
-                result[index++] = triangle[i][j];
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<n;j++) {
+                if (triangle[i][j] == 0) continue;
+                answer[index++] = triangle[i][j];
             }
         }
-
-        return result;
+        return answer;
     }
 }
