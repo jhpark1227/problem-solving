dc29952029ca32172b724492e667589ecbc14fec
Junhyeok Park <111430281+jhpark1227@users.noreply.github.com>
2026-09-23 11:56:57 +0900

    [level 1] Title: K번째수, Time: 3.71 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -2,15 +2,13 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-        int[] result = new int[commands.length];
-        for(int i=0;i<commands.length;i++){
-            List<Integer> numbers = new ArrayList<>();
-            for(int j=commands[i][0]-1;j<commands[i][1];j++){
-                numbers.add(array[j]);
-            }
-            Collections.sort(numbers);
-            result[i] = numbers.get(commands[i][2]-1);
+        List<Integer> answer = new ArrayList<>();
+        for(int[] command : commands) {
+            int[] arr = Arrays.copyOfRange(array, command[0] - 1, command[1]);
+            Arrays.sort(arr);
+            answer.add(arr[command[2] - 1]);
         }
-        return result;
+        
+        return answer.stream().mapToInt(i -> i).toArray();
     }
 }
