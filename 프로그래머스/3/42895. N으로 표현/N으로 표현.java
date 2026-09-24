a1549d9381835946fb0cc6a912fb0b1ce68fa838
Junhyeok Park <111430281+jhpark1227@users.noreply.github.com>
2026-09-24 11:38:00 +0900

    [level 3] Title: N으로 표현, Time: 13.61 ms, Memory: 87.6 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -2,27 +2,27 @@ import java.util.*;
 
 class Solution {
     public int solution(int N, int number) {
-        Set<Integer>[] dp = new Set[8 + 1];
-        for(int i=0;i<=8;i++) {
+        Set<Integer>[] dp = new Set[9];
+        for(int i=0;i<dp.length;i++) {
             dp[i] = new HashSet<>();
         }
-        for(int i=1;i<=8;i++) {
-            String tmp = "";
-            for(int j=0;j<i;j++) {
-                tmp += N;
-            }
-            dp[i].add(Integer.parseInt(tmp));
+        int concat = 0;
+        for(int i=1;i<=8;i++){
+            concat = concat * 10 + N;
+            dp[i].add(concat);
             for(int j=1;j<i;j++) {
-                for(int a : dp[j]) {
-                    for (int b : dp[i - j]) {
+                Set<Integer> set1 = dp[j];
+                Set<Integer> set2 = dp[i - j];
+                for(int a : set1) {
+                    for(int b : set2) {
                         dp[i].add(a + b);
                         dp[i].add(a - b);
                         dp[i].add(a * b);
                         if (b == 0) continue;
-                        dp[i].add(a / b);
+                        dp[i].add(a / b);       
                     }
                 }
-            }   
+            }
             if (dp[i].contains(number)) return i;
         }
         return -1;
