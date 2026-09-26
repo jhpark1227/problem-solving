f53f70e994866732d5d1e3b706e2c9ed377b48a5
Junhyeok Park <111430281+jhpark1227@users.noreply.github.com>
2026-09-26 12:16:45 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 3.20 ms, Memory: 88.9 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -1,54 +1,51 @@
 import java.util.*;
 
-import static java.lang.Thread.sleep;
-
 class Solution {
-    class Task{
-        int requestTime;
-        int jobTime;
-        Task(int requestTime, int jobTime){
-            this.requestTime = requestTime;
-            this.jobTime = jobTime;
-        }
-    }
     public int solution(int[][] jobs) {
-        Arrays.sort(jobs, new Comparator<>(){
-            public int compare(int[] a, int[] b){
-                if(a[0] == b[0]) return a[1] - b[1];
-                return a[0] - b[0];
+        List<Task> tasks = new ArrayList<>();
+        for(int i=0;i < jobs.length;i++) {
+            tasks.add(new Task(i, jobs[i][0], jobs[i][1]));
+        }
+        tasks.sort((a, b) -> a.requestTime - b.requestTime);
+        PriorityQueue<Task> pq = new PriorityQueue<>((a, b) -> {
+            if (a.duration != b.duration) {
+                return a.duration - b.duration;
             }
-        });
-        PriorityQueue<Task> pq = new PriorityQueue<>(new Comparator<>(){
-            public int compare(Task a, Task b){
-                return a.jobTime - b.jobTime;
+            if (a.requestTime != b.requestTime) {
+                return a.requestTime - b.requestTime;
             }
+            return a.number - b.number;
         });
-        Task now = null;
         int index = 0;
+        int sum = 0;
         int time = 0;
-        int count = jobs.length;
-        List<Integer> list = new ArrayList<>();
-        while(count > 0){
-            while(index < jobs.length && jobs[index][0] == time){
-                pq.add(new Task(jobs[index][0], jobs[index][1]));
+        int count = 0;
+        while(count< tasks.size()) {
+            while (index < tasks.size() && tasks.get(index).requestTime <= time) {
+                pq.add(tasks.get(index));
                 index++;
             }
-            if(now != null){
-                now.jobTime--;
-                if(now.jobTime == 0){
-                    list.add(time-now.requestTime);
-                    now = null;
-                    count--;
-                }
-            }
-
-            if(now == null){
-                if(!pq.isEmpty()) now = pq.poll();
-            }
-
+            if (!pq.isEmpty() && time >= pq.peek().requestTime) {
+                Task task = pq.poll();
+                time += task.duration;
+                sum += time - task.requestTime;
+                count++;
+                continue;
+            } 
             time++;
         }
-
-        return (int)list.stream().mapToInt(i->i).average().getAsDouble();
+        return sum / tasks.size();
+    }
+    
+    private static class Task {
+        int number;
+        int duration;
+        int requestTime;
+        
+        private Task(int number, int requestTime, int duration) {
+            this.number = number;
+            this.duration = duration;
+            this.requestTime = requestTime;
+        }
     }
 }
