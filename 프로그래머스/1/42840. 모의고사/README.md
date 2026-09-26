3a7a618ff13765f9137ba34f67796fe2f8aaab51
Junhyeok Park <111430281+jhpark1227@users.noreply.github.com>
2026-09-26 14:51:50 +0900

    [level 1] Title: 모의고사, Time: 7.79 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/README.md
+++ 프로그래머스/1/42840. 모의고사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 모의고사 - 42840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840) 
 
 ### 성능 요약
 
-메모리: 76.6 MB, 시간: 6.13 ms
+메모리: 79.7 MB, 시간: 7.79 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 29일 19:10:34
+2026년 09월 26일 14:51:45
 
 ### 문제 설명
 
--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,24 +1,24 @@
-import java.util.Arrays;
-import java.util.stream.IntStream;
+import java.util.*;
+import java.util.stream.*;
 
 class Solution {
-    final static int[] student1 = {1, 2, 3, 4, 5};
-    final static int[] student2 = {2, 1, 2, 3, 2, 4, 2, 5};
-    final static int[] student3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+    private final int[] bPattern = {1, 3, 4, 5};
+    private final int[] cPattern = {3, 1, 2, 4, 5};
+    
     public int[] solution(int[] answers) {
-        int[] scores = new int[3];
-
-        for(int i=0;i<answers.length;i++){
-            int student1Answer = student1[i%5];
-            int student2Answer = student2[i%8];
-            int student3Answer = student3[i%10];
-
-            if(student1Answer==answers[i]) scores[0]++;
-            if(student2Answer==answers[i]) scores[1]++;
-            if(student3Answer==answers[i]) scores[2]++;
+        int[] count = new int[3];
+        for(int i=0;i<answers.length;i++) {
+            int a = i % 5 + 1;
+            int b = i % 2 == 0 ? 2 : bPattern[i / 2 % 4];
+            int c = cPattern[i / 2 % 5];
+            if (a == answers[i]) count[0]++;
+            if (b == answers[i]) count[1]++;
+            if (c == answers[i]) count[2]++;
         }
-        int max = Arrays.stream(scores).max().getAsInt();
-
-        return IntStream.range(0,3).filter(i->scores[i]==max).map(i->i+1).toArray();
+        int max = Arrays.stream(count).max().getAsInt();
+        return IntStream.range(1, 4)
+            .filter(i -> count[i-1] == max)
+            .sorted()
+            .toArray();
     }
 }
